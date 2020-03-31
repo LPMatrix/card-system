f6caea85aac6cc4d2f6b6fb1021b70d1f2be5b2b
mubharaq <oluwadamilaremubarak@gmail.com>
2020-03-31 20:33:48 +0100

    tested the card component

--- src/app/agent/agent-dashboard/agent-dashboard.component.ts
+++ src/app/agent/agent-dashboard/agent-dashboard.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from 'src/app/shared/users.model';
 import { AgentService } from '../agent.service';
 import { AuthService } from 'src/app/auth/auth.service';
+import { Subject } from 'rxjs';
+declare var $: any;
 
 @Component({
   selector: 'app-agent-dashboard',
   templateUrl: './agent-dashboard.component.html',
   styleUrls: ['./agent-dashboard.component.css']
 })
-export class AgentDashboardComponent implements OnInit {
+export class AgentDashboardComponent implements OnInit, OnDestroy {
   users : User[] = [];
   userInformation : {name: string, image: string};
+  dtTrigger: Subject<any> = new Subject();
+  dtOptions: DataTables.Settings = {};
   counts: { userCount: number };
   constructor(private agentService : AgentService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2
+    };
     this.agentService.getUsers();
     this.getCount();
     this.userInformation = this.authService.getUserDetail();
     this.agentService.getUserStatusListener()
     .subscribe(responseData => {
       this.users = responseData;
+      this.dtTrigger.next();
     });
   }
 
@@ -33,6 +42,11 @@ export class AgentDashboardComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
   
 
