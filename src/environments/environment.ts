81676323207f068b36076d3a57d6b403f257a81f
Adedrey <adedireemmanuel@yahoo.com>
2020-09-21 02:46:16 +0100

    changed environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api/'
-  apiUrl: 'https://digicapture.herokuapp.com/api/'
+  // apiUrl: 'https://digicapture.herokuapp.com/api/'
+  apiUrl: 'https://esnbiometrics.com/api/'
 };
 
 /*
